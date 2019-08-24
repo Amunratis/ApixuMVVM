5dbaf38d5242a950411a56ff7490b4eaeb7050f5
amunratis <sirthesurfer@gmail.com>
2019-08-24 11:29:28 +0000

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ui/weather/future/detail/FutureDetailWeatherViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.ui.weather.future.detail
+
+import androidx.lifecycle.ViewModel
+
+class FutureDetailWeatherViewModel:ViewModel() {
+
+    //Todo implement ViewModel
+}
---
+++ app/src/main/java/com/example/myapplication/ui/weather/future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.ui.weather.future.list
+
+import androidx.lifecycle.ViewModel
+
+
+class FutureListWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
